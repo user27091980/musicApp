39c4a5dcf8e8603a65948a370af7eb65ab0d42be
Perfecto Lorenzo Couceiro <a14perfectolc@teis.intranet>
2025-10-21 17:25:51 +0200

    creados un data class y un campo de texto sin acabar

---
+++ app/src/main/java/com/example/musicapp/myComponents/componenteLogin.kt
@@ -0,0 +1,11 @@
+package com.example.musicapp.myComponents
+
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+
+@Composable
+fun componenteLogin ( Text: Text, ontextdate: (Text) -> Unit) {
+
+}
