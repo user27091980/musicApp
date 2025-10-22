d7950fa9c8ee869b9f4bbcdd0d0596ea97b296e1
Andrés <acanabalponcela@gmail.com>
2025-10-22 16:48:24 +0200

    top app bar

--- app/src/main/java/com/example/musicapp/myComponents/TopAppBarComponent.kt
+++ app/src/main/java/com/example/musicapp/myComponents/TopAppBarComponent.kt
@@ -1,18 +1,30 @@
 package com.example.musicapp.myComponents
 
+import android.R.attr.bitmap
 import android.annotation.SuppressLint
+import android.graphics.ImageFormat
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,11 +38,25 @@ fun TopBarraMain(){
 
     Scaffold(
         topBar = {
-            TopAppBar(
+
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
                 title = {
-                    Text(stringResource(R.string.top_bar))
+                    Text(stringResource(R.string.music_app))
                     Spacer(Modifier.width(16.dp))
-                    Icon(Icons.Default.CheckCircle, contentDescription = null)}
+
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
             )
         }
     ) { }
