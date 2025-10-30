cefa47968f310780e283c8441b4aa9ffc3e20853
Andrés <acanabalponcela@gmail.com>
2025-10-30 17:52:31 +0100

    pantalla de usuario más modificaciones cards problemas iconos y textfield en register screen

--- app/src/main/java/com/example/musicapp/myComponents/RegisterScreenTextFieldsComponents.kt
+++ app/src/main/java/com/example/musicapp/myComponents/RegisterScreenTextFieldsComponents.kt
@@ -1,6 +1,7 @@
 package com.example.musicapp.myComponents
 
 import android.R.attr.label
+import android.R.attr.singleLine
 import android.R.attr.text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldLabelScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -30,26 +32,29 @@ fun RegisterScreenTextFieldComponent() {
         Column {
 
            TextField(
-                value = text,
-                label = { Text(text = "usuario") },
-                singleLine = true,
-
-                /*onValueChange = {
+               value = String,
+               state = null,
+               label = {  },
+               singleLine = true,
+               /*onValueChange = {
                     text = it
                 }*/
-            )
+           )
 
             TextField(
-                value = text,
+                value = String,
                 label = { Text(text = "password") },
-                singleLine = true,
+                onValueChange: (String) -> Unit,
+
+                singleLine = true
                 /*onValueChange = {
                     text = it
                 }*/
             )
 
             TextField(
-                value = text,
+                value = String,
+                state
                 label = { Text(text = "email") },
                 singleLine = true,
                 /*leadingIcon = {
