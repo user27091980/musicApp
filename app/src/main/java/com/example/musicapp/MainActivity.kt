774c327858cf5b8d9327179aee6624a4f8e1e04c
Andrés <acanabalponcela@gmail.com>
2025-11-04 19:24:28 +0100

    arreglado lo del topBar en el scaffold

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -4,19 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.musicapp.myComponents.TopBar
 import com.example.musicapp.pages.DiscographyScreenPage
-import com.example.musicapp.pages.MusicBandScreenPage
 import com.example.musicapp.ui.theme.MusicAppTheme
 
 
