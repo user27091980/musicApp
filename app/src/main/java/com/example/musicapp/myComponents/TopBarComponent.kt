5400889d5a79e02576bce3745b183f1ccab8fe5d
Andrés <acanabalponcela@gmail.com>
2025-11-06 16:18:34 +0100

    menú desplegabl inicio

--- app/src/main/java/com/example/musicapp/myComponents/TopBarComponent.kt
+++ app/src/main/java/com/example/musicapp/myComponents/TopBarComponent.kt
@@ -1,12 +1,19 @@
 package com.example.musicapp.myComponents
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,13 +21,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.musicapp.R
 
 //componente de topAppBar
 @Composable
-fun TopBar(){
+fun TopBar() {
 
     var expanded by remember { mutableStateOf(false) }
 
@@ -35,27 +40,53 @@ fun TopBar(){
         ) {
             IconButton(onClick = {
                 expanded = true
+
             }) {
                 Icon(
-                    painter = painterResource(R.drawable.menu_50dp_e3e3e3_fill0_wght400_grad0_opsz48),
-                    contentDescription = "menu"
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "menú"
                 )
+
             }
-            //cuenta
-            IconButton(onClick = {/*do something*/ }) {
-                Icon(
-                    painter = painterResource(R.drawable.account_circle_50dp_e3e3e3_fill0_wght400_grad0_opsz48),
-                    contentDescription = "cuenta"
-                )
-            }
+
         }
     }
 }
 
 
 @Composable
-fun drow{}
-fun drow
+fun DropMenu() {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    // var isButtonClicked by remember { mutableStateOf(false) }
+
+    Column(Modifier.padding(8.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.TopEnd)
+        ) {
+            IconButton(onClick = { isExpanded = !isExpanded }) {
+
+            }
+            DropdownMenu(expanded = isExpanded, onDismissRequest = { isExpanded = false }) {
+                DropdownMenuItem(
+                    text = { Text(text = "inicio") },
+                    onClick = { })
+                DropdownMenuItem(
+                    text = { Text(text = "pérfil") },
+                    onClick = { })
+                DropdownMenuItem(
+                    text = { Text(text = "exit") },
+                    onClick = { })
+            }
+        }
+
+    }
+}
+
+
 
 
 
