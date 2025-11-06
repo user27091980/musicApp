d3cb99738a2ec29e14ffb89d95f9e3fcfc83bbea
Andrés <acanabalponcela@gmail.com>
2025-11-06 16:13:16 +0100

    menú desplegabl inicio

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -10,16 +10,10 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.musicapp.myComponents.BottomBar
 import com.example.musicapp.myComponents.TopBar
-import com.example.musicapp.pages.DiscographyScreenPage
-import com.example.musicapp.pages.MainScreenPage
 import com.example.musicapp.pages.MusicBandScreenPage
 import com.example.musicapp.ui.theme.MusicAppTheme
 
@@ -33,14 +27,14 @@ class MainActivity : ComponentActivity() {
 
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    topBar = { TopBar()},
+                    topBar = { TopBar(DropdownMenu()){} },
                     bottomBar = { BottomBar() },
                     content = { innerPadding ->
                         //SplashScreenPage()
                         //LoginScreenPage()
                         //RegisterScreenPage()
-                        MainScreenPage(modifier=Modifier.padding(innerPadding))
-                        //MusicBandScreenPage(modifier=Modifier.padding(innerPadding))
+                        //MainScreenPage(modifier=Modifier.padding(innerPadding))
+                        MusicBandScreenPage(modifier=Modifier.padding(innerPadding))
                         //UserInfoScreenPage(modifier=Modifier.padding(innerPadding))
                         //DiscographyScreenPage(modifier=Modifier.padding(innerPadding))
                     }
@@ -54,10 +48,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MusicAppPreviewPage() {
     MusicAppTheme {
-
-        DiscographyScreenPage()
+        //DiscographyScreenPage()
         //MainScreenPage()
-        //MusicBandScreenPage()
+        MusicBandScreenPage()
         //UserInfoScreenPage()
     }
 }
