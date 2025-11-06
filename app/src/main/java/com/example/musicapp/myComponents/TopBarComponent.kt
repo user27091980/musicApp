a7170889e375d0b16c0dd33c3d3ccb707acdab90
Andrés <acanabalponcela@gmail.com>
2025-11-06 16:18:34 +0100

    menú desplegabl inicio

--- app/src/main/java/com/example/musicapp/myComponents/TopBarComponent.kt
+++ app/src/main/java/com/example/musicapp/myComponents/TopBarComponent.kt
@@ -1,24 +1,92 @@
 package com.example.musicapp.myComponents
 
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 //componente de topAppBar
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar() {
-    //variable que se encarga del cambio de estado del menú,
-    DropDownMenu()
 
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(modifier = Modifier.fillMaxSize()) {
+        //cuenta
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 10.dp)
+                .wrapContentSize(align = Alignment.TopStart),
+            contentAlignment = Alignment.Center
+        ) {
+            IconButton(onClick = {
+                expanded = true
+
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "menú"
+                )
+
+            }
+
+        }
+    }
 }
 
 
 @Composable
-fun drow
+fun DropMenu() {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    // var isButtonClicked by remember { mutableStateOf(false) }
+
+    Column(Modifier.padding(8.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.TopEnd)
+        ) {
+            IconButton(onClick = { isExpanded = !isExpanded }) {
+
+            }
+            DropdownMenu(expanded = isExpanded, onDismissRequest = { isExpanded = false }) {
+                DropdownMenuItem(
+                    text = { Text(text = "inicio") },
+                    onClick = { })
+                DropdownMenuItem(
+                    text = { Text(text = "pérfil") },
+                    onClick = { })
+                DropdownMenuItem(
+                    text = { Text(text = "exit") },
+                    onClick = { })
+            }
+        }
+
+    }
+}
+
+
 
 
 
