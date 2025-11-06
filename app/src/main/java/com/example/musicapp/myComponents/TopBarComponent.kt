d1615f078eab1f4cecc5e2f9cbc8c82a873f8e91
Andrés <acanabalponcela@gmail.com>
2025-11-06 15:54:09 +0100

    menú desplegabl inicio

--- app/src/main/java/com/example/musicapp/myComponents/TopBarComponent.kt
+++ app/src/main/java/com/example/musicapp/myComponents/TopBarComponent.kt
@@ -1,19 +1,55 @@
 package com.example.musicapp.myComponents
 
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.musicapp.R
 
 //componente de topAppBar
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar() {
-    //variable que se encarga del cambio de estado del menú,
-    DropDownMenu()
+fun TopBar(){
 
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(modifier = Modifier.fillMaxSize()) {
+        //cuenta
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 10.dp)
+                .wrapContentSize(align = Alignment.TopStart),
+            contentAlignment = Alignment.Center
+        ) {
+            IconButton(onClick = {
+                expanded = true
+            }) {
+                Icon(
+                    painter = painterResource(R.drawable.menu_50dp_e3e3e3_fill0_wght400_grad0_opsz48),
+                    contentDescription = "menu"
+                )
+            }
+            //cuenta
+            IconButton(onClick = {/*do something*/ }) {
+                Icon(
+                    painter = painterResource(R.drawable.account_circle_50dp_e3e3e3_fill0_wght400_grad0_opsz48),
+                    contentDescription = "cuenta"
+                )
+            }
+        }
+    }
 }
 
 
