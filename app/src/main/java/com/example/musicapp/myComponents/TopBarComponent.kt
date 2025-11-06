53fd393e8b7cd79c5aa55b9732dfaa81148f3fc2
Andrés <acanabalponcela@gmail.com>
2025-11-06 16:10:20 +0100

    menú desplegabl inicio

--- app/src/main/java/com/example/musicapp/myComponents/TopBarComponent.kt
+++ app/src/main/java/com/example/musicapp/myComponents/TopBarComponent.kt
@@ -21,7 +21,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.musicapp.R
 
 //componente de topAppBar
 @Composable
@@ -44,15 +46,13 @@ fun TopBar() {
             }) {
                 Icon(
                     imageVector = Icons.Default.Menu,
-                    contentDescription = "menú"
+                    contentDescription = "More"
                 )
-
             }
 
         }
     }
 }
-
 
 
 @Composable
@@ -73,18 +73,21 @@ fun DropMenu() {
             }
             DropdownMenu(expanded = isExpanded, onDismissRequest = { isExpanded = false }) {
                 DropdownMenuItem(
-                    text = { Text(text = "inicio") },
+                    text = { Text(text = "") },
                     onClick = { })
                 DropdownMenuItem(
-                    text = { Text(text = "pérfil") },
+                    text = { Text(text = "") },
                     onClick = { })
                 DropdownMenuItem(
-                    text = { Text(text = "exit") },
+                    text = { Text(text = "") },
                     onClick = { })
             }
         }
+
     }
 }
+
+
 
 
 
