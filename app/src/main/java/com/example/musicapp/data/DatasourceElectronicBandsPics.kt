21e00600e8841b2876b2590cde81ec8f88d6be25
Andrés <acanabalponcela@gmail.com>
2025-11-18 10:53:28 +0100

    Nuevo componente lazyrow para bandas de música electrónica

--- app/src/main/java/com/example/musicapp/data/DatasourceElectronicBandsPics.kt
+++ app/src/main/java/com/example/musicapp/data/DatasourceElectronicBandsPics.kt
@@ -10,18 +10,15 @@ class DatasourceElectronicBandsPics {
     /*
     /*EStá clase es una función con una lista para el lazyy row  de la pagina bandScreen.*/
      */
-    class DatasourceIndustrialBandsPics {
+    fun LoadElectronicBands(): List<ElectBandsId> {
 
-        fun LoadElectBands():List<ElectBandsId>{
+                    return listOf(
 
-            return listOf(
+            ElectBandsId(R.drawable.venetiansnares),
+            ElectBandsId(R.drawable.w52do26gtpata72nfuesnt_1772_80),
+            ElectBandsId(R.drawable.boards_20of_20canada_20new_20mix_20warp),
+            ElectBandsId(R.drawable.a1664460568_10)
 
-                ElectBandsId(R.drawable.venetiansnares),
-                ElectBandsId (R.drawable.w52do26gtpata72nfuesnt_1772_80),
-                ElectBandsId(R.drawable.boards_20of_20canada_20new_20mix_20warp),
-                ElectBandsId(R.drawable.a1664460568_10)
-
-            )
-        }
+        )
     }
 }
