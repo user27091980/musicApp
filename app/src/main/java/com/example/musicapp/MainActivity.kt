bf1b1f9092c454d28c84723e04692078010ac7d9
Andrés <acanabalponcela@gmail.com>
2025-11-29 13:49:58 +0100

    ajustadas las cards

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -45,8 +45,8 @@ class MainActivity : ComponentActivity() {
                     bottomBar = { BottomBar() },
                     content = { innerPadding ->
                         //MainScreenPage(modifier = Modifier.padding(innerPadding))
-                        //BandAe(modifier=Modifier.padding(innerPadding))
-                        BandAphx(modifier=Modifier.padding(innerPadding))
+                        BandAe(modifier=Modifier.padding(innerPadding))
+                        //BandAphx(modifier=Modifier.padding(innerPadding))
                         //BandBoc(modifier=Modifier.padding(innerPadding))
                         //BandKyuss(modifier=Modifier.padding(innerPadding))
                         //BandTool(modifier=Modifier.padding(innerPadding))
@@ -69,8 +69,8 @@ fun MusicAppPreviewPage() {
         //LoginScreenPage()
         //RegisterScreenPage()
         //MainScreenPage()
-        //BandAe()
-        BandAphx()
+        BandAe()
+        //BandAphx()
         //BandBoc()
         //BandKyuss()
         //BandTool()
