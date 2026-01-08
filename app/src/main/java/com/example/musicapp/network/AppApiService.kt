9692f2d0764183d1be401eb0bbb3b235087f42dd
Andrés <acanabalponcela@gmail.com>
2026-01-08 18:57:06 +0100

    lib.versions changed retrofit

---
+++ app/src/main/java/com/example/musicapp/network/AppApiService.kt
@@ -0,0 +1,13 @@
+package com.example.musicapp.network
+
+interface AppApiService {
+/*RUTA
+
+    @GET(ruta)
+    //@Path en la ruta que hemos definido arriba va a coger el id que pasamos por parámetro y lo escribmos.
+    //toda acción enviada por un retrofit devuelve un objeto response.
+    suspend fun getXXXXXX(@Path("id") id:Int):Response<nombreResponse>
+
+ */
+}
+//crearemos un dataclass con el nombre del nombreREsponse
