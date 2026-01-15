6c9e568dcd112ab4455abeba552bcbf793c34976
Andrés <acanabalponcela@gmail.com>
2026-01-15 17:15:31 +0100

    navRoutes arreglado

--- app/src/main/java/com/example/musicapp/myComponents/ButtonComponent.kt
+++ app/src/main/java/com/example/musicapp/myComponents/ButtonComponent.kt
@@ -3,9 +3,7 @@ package com.example.musicapp.myComponents
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
