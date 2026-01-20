adb5eb4f3c684219820db97e3e8fbc58dda8259e
Andrés <acanabalponcela@gmail.com>
2026-01-20 17:44:29 +0100

    añadidos documentación

---
+++ app/src/main/java/com/example/musicapp/data/remote/dto/ModifyResponse.kt
@@ -0,0 +1,15 @@
+package com.example.musicapp.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.SerialName
+
+data class ModifyResponse (
+
+    @SerializedName("id")
+    val id: String,
+
+    @SerializedName("message")
+    val message: String,
+
+
+)
---
+++ app/src/main/java/com/example/musicapp/model/Usuario.kt
@@ -0,0 +1,4 @@
+package com.example.musicapp.model
+
+class Usuario {
+}
