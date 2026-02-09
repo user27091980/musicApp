8815ab5196166e8419de12524b2d61bdb771aa88
Andrés <acanabalponcela@gmail.com>
2026-02-09 07:46:07 +0100

    comentarios

--- app/src/main/java/com/example/musicapp/view/pages/MainPage.kt
+++ app/src/main/java/com/example/musicapp/view/pages/MainPage.kt
@@ -1,20 +1,25 @@
 package com.example.musicapp.view.pages
 
+//compoose imports
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+//navigation
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+//reused lazycolumncomponent
 import com.example.musicapp.view.myComponents.LazyColumnComponent
 
 /**
  * @author="Andrés"
- * @param
- * @function
+ * @param navController navigation controller for changes between screens
+ * @param modifier modifier for layout personalization from outside
  */
 @Composable
 fun MainScreenPage(navController: NavController,modifier: Modifier = Modifier) {
 
+    //Component which shows a content LazyColumn
+    //it passed a Navcontroller for the navigation between items
     LazyColumnComponent(
         navController = NavController,
         modifier = modifier
@@ -22,6 +27,9 @@ fun MainScreenPage(navController: NavController,modifier: Modifier = Modifier) {
 
 }
 
+/**
+ * MainScreen preview. It doesn´t executes on the app, only on the preview
+ */
 @Preview
 @Composable
 fun MainPrev() {
--- app/src/main/java/com/example/musicapp/viewmodel/vm/LoginVM.kt
+++ app/src/main/java/com/example/musicapp/viewmodel/vm/LoginVM.kt
@@ -1,9 +1,13 @@
 package com.example.musicapp.viewmodel.vm
 
+//Viewmodel for lifecycle-bound coroutines
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+//Repository that manages autenthication
 import com.example.musicapp.data.repository.AuthRepository
+//UI login State
 import com.example.musicapp.viewmodel.uistate.LoginUiState
+//kotlin´s flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -13,30 +17,66 @@ import kotlinx.coroutines.launch
 
 
 /**
+ * Viewmodel that manages the login screen login.
+ *
+ * - uses the UI state (email, password, loading, errors)
+ * - for emited events of only one time (navigation)
+ *
  * @author Andrés
  *
  */
 
-// In LoginVM.kt
+//Authenticaion repository
 class LoginViewModel(private val authRepository: AuthRepository) : ViewModel() {
-
+    /**
+     * UI mutable internal state only viewmodel can modified it
+     */
     private val _uiState = MutableStateFlow(LoginUiState())
+
+    /**
+     * only read UI state. UI views it with collectAsSTate()
+     */
     val uiState = _uiState.asStateFlow()
+
+    /**
+     * one-shot events, for navigation, etc
+     */
     private val _events = MutableSharedFlow<LoginEvent>()
+
+    /**
+     * secure exposition of events flow
+     */
     val events = _events.asSharedFlow()
 
+    /**
+     * Updates the email when user writes
+     */
     fun onEmailChange(newEmail: String) {
         _uiState.update { it.copy(email = newEmail) }
     }
 
+    /**
+     * UPdates password when user writes it
+     */
     fun onPasswordChange(newPassword: String) {
         _uiState.update { it.copy(password = newPassword) }
     }
 
+    /**
+     * alternates the password visibility
+     */
     fun togglePasswordVisibility() {
         _uiState.update { it.copy(passwordVisible = !it.passwordVisible) }
     }
 
+    /**
+     * Executes login process
+     *
+     * -Activates the load state
+     * -Call to the repository
+     * -Broadcast a navigation event if it´s success
+     * -manage errors
+     */
     fun login() {
         viewModelScope.launch {
             _uiState.update {
@@ -70,11 +110,19 @@ class LoginViewModel(private val authRepository: AuthRepository) : ViewModel() {
     }
 }
 
-// Mejor declarar el evento fuera del VM
+/**
+ * one single execution for login screen
+ * it´s positioned outside the viewmodel for mayor clarity
+ */
 sealed interface LoginEvent {
+    //Event for navigation to main screen
     object NavigateToHome : LoginEvent
 }
 
+/**
+ * Authenticacion Repository interface
+ * Allows disengage the logic from the ViewMOdel
+ */
 interface AuthRepo {
     suspend fun login(email: String, password: String)
 }
