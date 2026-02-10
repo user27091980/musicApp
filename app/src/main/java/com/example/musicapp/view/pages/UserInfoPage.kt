12f688f36dd0cf12eadd010ce3b56b78a9800db9
Andrés <acanabalponcela@gmail.com>
2026-02-10 12:32:50 +0100

    comentarios de codigo y añadidos enlaces de los discos de la fila depslazable de cada banda

--- app/src/main/java/com/example/musicapp/view/pages/UserInfoPage.kt
+++ app/src/main/java/com/example/musicapp/view/pages/UserInfoPage.kt
@@ -1,8 +1,12 @@
 package com.example.musicapp.view.pages
 
+//imports row, for horizontal row organization
 import androidx.compose.foundation.layout.Row
+//import for obtain access to the Material 3 scheme colors
 import androidx.compose.material3.MaterialTheme
+//import Composable
 import androidx.compose.runtime.Composable
+//
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.musicapp.styles.cameraMod
